634cd48ad512b532b925e2fa777dde975f34c362
Oliver <68897677+oli208@users.noreply.github.com>
2022-04-13 15:09:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ import os
 import json
 
 jsondata = {}
+stop = False
 
+# filepath = "E:\Oliver\oliver_drive\Programmieren\python\py_obsidian"
+# filepath = "/mnt/c/Users/Oliver/Desktop/Chem"
 obsidianPath = "E:\Oliver\oliver_drive\zettelkasten\.obsidian"
 directory = "obsidian-graphview-presets"
 
@@ -13,49 +16,72 @@ plugindir = pathtoplugins + "\\" + directory
 
 
 def setupPlugindir():
-    print('TODO')
+    try:
+        os.mkdir(os.path.join(pathtoplugins, directory))
+        print("Directory '% s' created" % directory)
+
+    except FileExistsError:  # not really an error
+        print("Pluginfolder exists")
+
 
 def readGraphsettings():
-    print('TODO')
+    # read the graphsettings form file
+    try:
+        # Opening JSON file
+        with open(obsidianPath + "\graph.json", 'r', encoding='UTF-8') as f:
+            return json.load(f)
+    except FileNotFoundError:
+        print("Graphfile not found")
+
 
 def storeGraphsettings(settings):
-    print('TODO')
+    # save actual settings
+    with open(plugindir + "\data.json", 'w+') as f:
+        json.dump(jsondata, f)
 
-def getstoredsettings(name):
-    print('TODO')
+
+def loadsettings(name):
+    # read data
+    with open(plugindir + "\data.json", 'r') as f:
+        data = json.load(f)
+
+    for i in range(len(data)):
+        redname = data.get(str(i)).get("name")
+        if redname == name:
+            return data.get(str(i)).get("settings")
+
 
 def loadGraphsettings(settings):
-    print('TODO')
+    with open(obsidianPath + "\graph.json", 'w', encoding='UTF-8') as f:
+        json.dump(settings, f)
 
 
-try:
-    os.mkdir(os.path.join(pathtoplugins, directory))
-    print("Directory '% s' created" % directory)
+while not stop:
+    try:
+        mode = int(input("Was möchtest du machen? \n save settings: 1\n load settings: 2\n Beenden: 3\n"))
 
-except FileExistsError: # not really an error
-    print("Pluginfolder exists")
+    except:
+        print("ERROR")
+        continue
 
-# read the graphsettings form file
-try:
-    # Opening JSON file
-    with open(obsidianPath + "\graph.json", 'r', encoding='UTF-8') as f:
-        actsettings = json.load(f)
+    if mode == 1:
+        name = input("Gib einen Namen ein:")
+        setg = readGraphsettings()
+        jsondata[0] = {"name": name, "settings": setg}  # FIXME
+        storeGraphsettings(setg)
+    elif mode == 2:
+        name = input("Gib den Namen ein: ") #TODO check if aviable
+        setg = loadsettings(name)
+        loadGraphsettings(setg)
+    elif mode == 3:
+        stop = True
+        print("Das Programm wird beendet")
+    else:
+        print("undefined action")
 
-except FileNotFoundError:
-    print("Graphfile not found")
-
-jsondata[0] = {"name": "firstname", "settings": actsettings}
-
-print(json.dumps(jsondata, indent=4))
-
-# save actual settings
-with open(plugindir + "\data.json", 'w+') as f:
-    json.dump(jsondata, f)
-
-
-# read data
-with open(plugindir + "\data.json", 'r') as f:
-    data = json.load(f)
+# print(json.dumps(jsondata, indent=4))
 
 
+set = loadsettings("firstname")
 
+loadGraphsettings(set)
